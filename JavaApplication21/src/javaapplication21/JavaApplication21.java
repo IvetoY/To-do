974e36b06f196a95f57c45af80f5f77f9d1773c5
IvetoY <ivelinabeti@gmail.com>
2023-05-04 19:07:24 +0300

    PROBA

--- JavaApplication21/src/javaapplication21/JavaApplication21.java
+++ JavaApplication21/src/javaapplication21/JavaApplication21.java
@@ -16,8 +16,7 @@ public class JavaApplication21 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello world");
-        System.out.println("727272");
-        System.out.println("66566");
+        //12
     }
     
 }
